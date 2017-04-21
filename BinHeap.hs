4c77a833c497ba6670d803f84ff35d76c185048a
AlexandrZamiralov <ff.akila@mail.ru>
2017-04-21 21:47:31 +0400

    Update BinHeap.hs

--- BinHeap.hs
+++ BinHeap.hs
@@ -6,6 +6,13 @@ type BinHeap e = [BinTree e]
 add :: Ord e => e -> BinHeap e -> BinHeap e
 add a b = (singletonHeap a) `unionBinHeaps` b
 
+replace :: Ord e => e -> BinHeap e -> BinHeap e
+replace _ [] = []
+replace v [x] = [replaceT v x]
+replace v ((BinTree k next):(BinTree k1 next1):xs)
+  | k < k1 = (BinTree k1 next1):(replace v ((BinTree k next):xs))
+  | otherwise = (BinTree k next):(replace v ((BinTree k1 next1):xs))
+
 fromList :: Ord e => [e] -> BinHeap e
 fromList p = foldr (add) [] p
 
@@ -38,3 +45,20 @@ mergeT' :: Ord e => BinTree e -> BinTree e -> BinTree e
 mergeT' xx@(BinTree x xs) yy@(BinTree y ys)
   | x > y = (BinTree y (xx:ys))
   | otherwise = (BinTree x (yy:xs))
+  
+replaceT :: Ord e => e -> BinTree e -> BinTree e
+replaceT v (BinTree e next)
+  | v <= e = BinTree v next
+  | otherwise = balanceT (BinTree v next)
+  
+balanceT :: Ord e => BinTree e -> BinTree e
+balanceT = balanceT'
+  where
+    balanceT' (BinTree e []) = BinTree e []
+    balanceT' b@(BinTree v [(BinTree e next)])
+      | v <= e = b
+      | otherwise = (BinTree e [balanceT' (BinTree v next)])
+    balanceT' b@(BinTree v ((BinTree e next):(BinTree e1 next1):xs))
+      | v <= (min e e1) = b
+      | e < v = (BinTree e ((balanceT' (BinTree v next)):(BinTree e1 next1):xs))
+      | otherwise = (BinTree e1 ((BinTree e next):(balanceT'(BinTree v next1)):xs))
